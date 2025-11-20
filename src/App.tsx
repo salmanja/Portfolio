d00f43cc7f28147b3c04e31e355d8506b8d1d018
salmanja <71104867+salmanja@users.noreply.github.com>
2025-11-20 15:15:55 -0500

    -changed the default camera to orthographic and init OrbitControls         -cleaned up imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,55 @@
 import { Canvas } from "@react-three/fiber";
-import { useFrame } from "@react-three/fiber";
+import { useState } from "react";
+import { OrbitControls, OrthographicCamera} from '@react-three/drei'
 
-function MyAnimatedSphere() {
-  useFrame(() => {
-    // setup animation 
-    console.log("Frame rendered");
-  });
+// function MyAnimatedSphere() {
+//   useFrame(() => {
+//     // setup orbit controls animation 
+//     console.log("Frame rendered");
+//   });
 
-  return (
-    <>
-      <mesh>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshStandardMaterial />
-      </mesh>
-      <ambientLight intensity={0.1} />
-      <directionalLight position={[0, 0, 5]} color="pink" />
-    </>
-  );
-};
+//   return (
+//     <>
+//       <mesh>
+//         <boxGeometry args={[2, 2, 2]} />
+//         <meshStandardMaterial />
+//       </mesh>
+//       <ambientLight intensity={0.1} />
+//       <directionalLight position={[0, 0, 5]} color="pink" />
+//     </>
+//   );
+// };
 
 function App() {
+const [isDragging, setIsDragging] = useState(false);
+
   return (
-      <>
+    <div style={{ width: '100vw', height: '100vh' }}>
       <Canvas>
-       <MyAnimatedSphere />
+        {/* setup the orthographic default camera */}
+        <OrthographicCamera 
+        makeDefault
+        position={[0, 40, 200]}
+        zoom={1}
+        left={-5}
+        right={5}
+        top={5}
+        bottom={-5}
+        />
+        {/* orbit controls for mouse control */}
+        <OrbitControls
+        minZoom={10}
+        maxZoom={50}
+        enabled={!isDragging}
+        />
+        <mesh>
+          <boxGeometry args={[2,2,2]} />
+          <meshStandardMaterial />
+        </mesh>
+        <ambientLight intensity={0.1} />
+        <directionalLight position={[5, 5, 5]} color="pink" />
       </Canvas>
-      </>
+    </div>
   );
 }
 
