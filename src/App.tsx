169b4d4e8b31e3be39112a599893d81694081130
salmanja <71104867+salmanja@users.noreply.github.com>
2025-11-19 23:04:40 -0500

    created first scene and will be testing animations and hooks!

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-function App() {
+import { Canvas } from "@react-three/fiber";
+import { useFrame } from "@react-three/fiber";
+
+function MyAnimatedSphere() {
+  useFrame(() => {
+    // setup animation 
+    console.log("Frame rendered");
+  });
+
   return (
     <>
-      Hello World
+      <mesh>
+        <boxGeometry args={[2, 2, 2]} />
+        <meshStandardMaterial />
+      </mesh>
+      <ambientLight intensity={0.1} />
+      <directionalLight position={[0, 0, 5]} color="pink" />
     </>
-  )
+  );
+};
+
+function App() {
+  return (
+      <>
+      <Canvas>
+       <MyAnimatedSphere />
+      </Canvas>
+      </>
+  );
 }
 
-export default App
+export default App;
