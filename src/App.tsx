f1957d3fb9ec827eccbc9da3c905d27c221c15ed
salmanja <71104867+salmanja@users.noreply.github.com>
2025-12-17 17:01:00 -0500

    testing my first scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Canvas } from "@react-three/fiber";
 import { useState } from "react";
-import { OrbitControls, OrthographicCamera} from '@react-three/drei';
+import { Suspense } from "react";
+import { OrbitControls} from '@react-three/drei';
+import FirstScene from "./components/testing/FirstScene";
 
 function App() {
 const [isDragging, _setIsDragging] = useState(false);
 
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div style={{ width: "100vw", height: "100vh" }}>
       <Canvas>
-        {/* setup the orthographic default camera */}
+        {/* setup the orthographic default camera 
         <OrthographicCamera 
         makeDefault
         position={[0, 40, 200]}
@@ -18,7 +20,7 @@ const [isDragging, _setIsDragging] = useState(false);
         top={5}
         bottom={-5}
         />
-        {/* orbit controls for mouse control */}
+        {/* orbit controls for mouse control 
         <OrbitControls
         minZoom={10}
         maxZoom={50}
@@ -28,6 +30,13 @@ const [isDragging, _setIsDragging] = useState(false);
           <boxGeometry args={[2,2,2]} />
           <meshStandardMaterial />
         </mesh>
+        */}
+        <OrbitControls minZoom={10} maxZoom={50} enabled={!isDragging} autoRotate/>
+
+        <Suspense fallback={null}>
+          <FirstScene />
+        </Suspense>
+        
         <ambientLight intensity={0.1} />
         <directionalLight position={[5, 5, 5]} color="pink" />
       </Canvas>
