4fe42cb1401cd17ba338f312a4e8985ef36e9bb8
salmanja <71104867+salmanja@users.noreply.github.com>
2026-01-01 00:58:50 -0500

    Experimented with camera angles, scaling and rotation of mesh.

--- src/components/FloorPane.tsx
+++ src/components/FloorPane.tsx
@@ -1,10 +1,15 @@
-function FloorPane () {
+function FloorPane() {
 
-return (
-  <mesh rotation={[-Math.PI / 2, 0,0 ]} position={[0, -2, 0]} receiveShadow>
-    <planeGeometry args={[5, 5]} />
-    <meshStandardMaterial color="#303b00" />
-  </mesh>
-);
+  return (
+      <mesh
+        rotation={[-Math.PI / 2, 0, 0]}
+        position={[0, -2, 0]}
+        scale={[0.5, 2.0, 1.0]}
+        receiveShadow
+      >
+        <planeGeometry args={[5, 5]} />
+        <meshStandardMaterial color="#303b00" />
+      </mesh>
+  );
 }
 export default FloorPane;
