3d34c3451da814fd858b41a3d971bb9b03378c22
salmanja <71104867+salmanja@users.noreply.github.com>
2026-01-07 18:23:35 -0500

    1. Made plane relative to the camera position while scrolling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import { Canvas } from "@react-three/fiber";
-import { useState } from "react";
 import { Suspense } from "react";
-import { OrbitControls} from '@react-three/drei';
+import {OrbitControls, ScrollControls} from '@react-three/drei';
 import FloorPane from "./components/FloorPane";
 
 function App() {
-const [isDragging, _setIsDragging] = useState(false);
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <Canvas>
-        <OrbitControls enabled={isDragging} />
+        <ScrollControls pages={2} damping={0.1}>
+          <OrbitControls enabled={false} />
 
-        <Suspense fallback={null}>
-          <FloorPane />
-        </Suspense>
-        <ambientLight intensity={2} />
-        <directionalLight position={[10, 10, 5]} intensity={1} />
+          <Suspense fallback={null}>
+            <FloorPane />
+          </Suspense>
+
+          <ambientLight intensity={2} />
+          <directionalLight position={[10, 10, 5]} intensity={1} />
+        </ScrollControls>
       </Canvas>
     </div>
   );
--- src/components/FloorPane.tsx
+++ src/components/FloorPane.tsx
@@ -1,15 +1,42 @@
+import { useThree, useFrame } from "@react-three/fiber";
+import { useScroll } from "@react-three/drei";
+import { useRef } from "react";
+import * as THREE from "three";
+
 function FloorPane() {
+  const scroll = useScroll();
+  const planeRef = useRef<THREE.Mesh | null>(null);
+  const { camera } = useThree();
+
+  useFrame(() => {
+    const offset = scroll.offset;
+
+    const cameraDirection = new THREE.Vector3();
+    camera.getWorldDirection(cameraDirection);
+
+    const moveDistance = offset * 20;
+
+    const plane = planeRef.current;
+    if (!plane) return;
+
+    const target = camera.position
+      .clone()
+      .add(cameraDirection.multiplyScalar(moveDistance));
+      target.y = -1;
+      plane.position.lerp(target, 0.5);
+  });
 
   return (
-      <mesh
-        rotation={[-Math.PI / 2, 0, 0]}
-        position={[0, -2, 0]}
-        scale={[0.5, 2.0, 1.0]}
-        receiveShadow
-      >
-        <planeGeometry args={[5, 5]} />
-        <meshStandardMaterial color="#303b00" />
-      </mesh>
+    <mesh
+      ref={planeRef}
+      rotation={[-Math.PI / 2, 0, 0]}
+      position={[0, -2, 0]}
+      scale={[0.5, 2.0, 1.0]}
+      receiveShadow
+    >
+      <planeGeometry args={[5, 5]} />
+      <meshStandardMaterial color="#303b00" />
+    </mesh>
   );
 }
 export default FloorPane;
