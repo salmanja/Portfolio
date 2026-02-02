6a1d31a11bddc49a093e575f69f129eeffc58b10
salmanja <71104867+salmanja@users.noreply.github.com>
2026-02-02 15:36:32 -0500

    1. installed MUI 2. Created the About Me panel for testing rendering.  3. Cleaned up the CSS 4. single source of truth state 5. conditional rendering with mock button

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
-import {OrbitControls, ScrollControls} from '@react-three/drei';
+import { Suspense, useState } from "react";
+import { OrbitControls, ScrollControls } from "@react-three/drei";
 import Trail from "./ThreeD components/Trail";
+import { Button } from "@mui/material";
+import AboutPanel from "./UI components/Panels/About Me Panel/AboutPanel";
 
 function App() {
+ // all my components array
+
+  const [isActivePanel, setIsActivePanel] = useState(false);
+
+  const togglePanel = () =>{
+    setIsActivePanel(!isActivePanel);
+  }
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
+
+
+      {/* mock button to toggle panel/null */}
+      <Button color="primary" onClick={togglePanel}>
+        {isActivePanel ? "Hide Panel" : "Show Panel"}
+      </Button>
+
+      {isActivePanel ? <AboutPanel /> : null}
+
+
       <Canvas>
         <ScrollControls pages={4} damping={0.1}>
           <OrbitControls enabled={false} />
 
           <Suspense fallback={null}>
-            <Trail/>
+            <Trail />
           </Suspense>
 
           <ambientLight intensity={2} />
